9bb1a4fad1d9edd58a2b981ffff5650041f350e8
kohnakagawa <tsunekou1019@gmail.com>
2015-12-27 21:54:03 +0900

    time measurement function is added.

--- test/prng_test/prng_test.cpp
+++ test/prng_test/prng_test.cpp
@@ -4,9 +4,15 @@
 #include "f_calculator.hpp"
 #include <numeric>
 #include <ctime>
+#include <sys/time.h>
+#include <iomanip>
 
-constexpr PS::U32 sample_n = 200;
-constexpr PS::U32 all_time = 200;
+constexpr PS::U32 sample_n = 100;
+constexpr PS::U32 all_time = 100;
+
+double time_diff(timeval& tv1, timeval& tv2) {
+  return tv2.tv_sec - tv1.tv_sec + (tv2.tv_usec - tv1.tv_usec) * 1.0e-6;
+}
 
 void gen_random_val(PS::F64* buffer, const PS::U32 m_seed) {
   PS::U32 cnt = 0;
@@ -14,7 +20,7 @@ void gen_random_val(PS::F64* buffer, const PS::U32 m_seed) {
     for(PS::U32 i = 0; i < sample_n; i++) {
       for(PS::U32 j = i + 1; j < sample_n; j++) {
 	Saru saru(i, j, time + m_seed);
-	const PS::F64 buf = std::sqrt(-2.0f * std::log(saru.f() ) ) * std::cos(2.0f * M_PI * saru.f());
+	const PS::F64 buf = saru.nrml();
 	buffer[cnt++] = buf;
       }
     }
@@ -29,8 +35,16 @@ int main() {
   buffer = new PS::F64 [buf_size];
   buffer_base = new PS::F64 [buf_size];
 
+  timeval tv1, tv2;
+  gettimeofday(&tv1, NULL);
   gen_random_val(buffer, m_seed); //first
-  gen_random_val(buffer_base, m_seed); //again genrate
+  gettimeofday(&tv2, NULL);
+
+  std::cout << "generation time\n";
+  std::cout << std::setprecision(15);
+  std::cout << time_diff(tv1, tv2) << std::endl;;
+
+  gen_random_val(buffer_base, m_seed); //genrate again
   
   for(PS::U32 i = 0; i < buf_size; i++)
     assert(buffer_base[i] == buffer[i]);
