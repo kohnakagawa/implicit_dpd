5b511525666fddc7f726b445951d6cf42310f6f2
kohnakagawa <tsunekou1019@gmail.com>
2016-01-18 16:00:48 +0900

    add cm vel removal routine.

--- parameter.hpp
+++ parameter.hpp
@@ -293,6 +293,17 @@ public:
   }
 
   template<class Tpsys>
+  void RemoveCMDrift(Tpsys& sys) const {
+    PS::F64vec cmvel(0.0, 0.0, 0.0);
+    const PS::S32 num = sys.getNumberOfParticleLocal();
+    for(PS::S32 i = 0; i < num; i++)
+      cmvel += sys[i].vel;
+    cmvel /= num;
+    for(PS::S32 i = 0; i < num; i++)
+      sys[i].vel -= cmvel;
+  }
+
+  template<class Tpsys>
   PS::U32 LoadParticleConfig(Tpsys& sys) const {
     const std::string fname = cdir + "/init_config.xyz";
     FILE* fp = io_util::xfopen(fname.c_str(), "r");
@@ -305,6 +316,8 @@ public:
       PS::Abort();
     }
     fclose(fp);
+
+    RemoveCMDrift(sys);
     return cur_time;
   }
 
