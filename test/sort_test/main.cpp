cf994c5cd1da6d5bc45ae285eb4d1d514d0b2a59
kohnakagawa <tsunekou1019@gmail.com>
2016-02-10 18:46:46 +0900

    benchmark test for tbb gnu_parallel_sort is added.

--- test/sort_test/main.cpp
+++ test/sort_test/main.cpp
@@ -1,29 +1,51 @@
 #include <iostream>
 #include <ctime>
 #include <cstdlib>
-#include "particle_simulator.hpp"
+#include <algorithm>
+#include <sys/time.h>
+#include <parallel/algorithm>
+#include <cassert>
+#include <tbb/parallel_sort.h>
 
-struct val {
-  PS::U32 key;
-  PS::U32 getKey() {
-    return key;
-  }
-};
+double time_diff(timeval& tv1, timeval& tv2) {
+  return tv2.tv_sec - tv1.tv_sec + (tv2.tv_usec - tv1.tv_usec) * 1.0e-6;
+}
 
-const int num = 100;
-val v[num], v_buf[num];
+typedef unsigned int uint;
+
+template<typename Dtype>
+void check(Dtype* array, const uint size) {
+  for (uint i = 0; i < size - 1; i++)
+    assert(array[i] <= array[i + 1]);
+}
 
 int main() {
+  const int num = 1000000;
+  uint *v0 = new uint [num];
+  uint *v1 = new uint [num];
+  
   srand((size_t)time(NULL));
   for(int i = 0; i < num; i++)
-    v[i].key = rand();
+    v1[i] = v0[i] = (uint) rand();
+
+  timeval tv1, tv2, tv3, tv4;
   
-  PS::RadixSort<PS::U32> rsorter;
-  rsorter.lsdSort(v, v_buf, 0, num);
+  gettimeofday(&tv1, nullptr);
+  // __gnu_parallel::sort(v0, v0 + num);
+  tbb::parallel_sort(v0, v0 + num);
+  gettimeofday(&tv2, nullptr);
 
-  for(int i = 0; i < num; i++)
-    std::cout << v[i].key << std::endl;
+  printf("%f\n", time_diff(tv1, tv2));
 
-  for(int i = 0; i < num - 1; i++)
-    assert(v[i].key <= v[i + 1].key);
+  gettimeofday(&tv3, nullptr);
+  std::sort(v1, v1 + num);
+  gettimeofday(&tv4, nullptr);
+
+  printf("%f\n", time_diff(tv3, tv4));
+  
+  check(v0, num);
+  check(v1, num);
+
+  delete [] v0;
+  delete [] v1;
 }
