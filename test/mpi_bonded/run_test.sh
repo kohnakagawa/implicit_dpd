7b515dd98102223650078ece0723e6e8a6d94302
kohnakagawa <tsunekou1019@gmail.com>
2016-02-10 11:39:43 +0900

    bonded test routines are added.

--- test/mpi_bonded/run_test.sh
+++ test/mpi_bonded/run_test.sh
@@ -1,17 +1,15 @@
 #!/bin/sh
 export OMP_NUM_THREADS=4
 
-#dir=random
-#dir=flat
-dir=cylind
-#dir=sphere
-
+dir=31x31
+# dir=32x32
+# dir=complex
 
 mpiexec -np 4 ./mpi_bonded.out ./result_mpi/$dir
-./bonded.out ./result_single/$dir
+./bonded.out ./result/$dir
 
 awk '{if(NR >= 3) print $0}' ./result_mpi/$dir/traject.xyz > tmp_mpi.txt
-awk '{if(NR >= 3) print $0}' ./result_single/$dir/traject.xyz > tmp.txt
+awk '{if(NR >= 3) print $0}' ./result/$dir/traject.xyz > tmp.txt
 sort -n -k5 tmp_mpi.txt > sort.txt
 if cmp -s sort.txt tmp.txt; then
    echo Success test.
@@ -19,21 +17,21 @@ else
    echo Different. Show maximum error.
 fi
 
-awk '{print $15, $16, $17}' sort.txt > 1.txt
-awk '{print $15, $16, $17}' tmp.txt > 2.txt
+awk '{print $2, $3, $4, $15, $16, $17}' sort.txt > 1.txt
+awk '{print $2, $3, $4, $15, $16, $17}' tmp.txt > 2.txt
 
 R --vanilla --slave <<EOF
 # check acc
 print("acc error")
 dat_mpi <- data.matrix(read.table("1.txt"))
 dat     <- data.matrix(read.table("2.txt"))
-diff    <- abs(dat_mpi - dat)
+diff    <- abs(dat_mpi - dat) / abs(dat_mpi)
 max(diff)
 
 print("pressure error")
 dat_mpi <- data.matrix(read.table("./result_mpi/$dir/pressure.txt"))
 dat     <- data.matrix(read.table("./result/$dir/pressure.txt"))
-diff    <- abs(dat_mpi - dat)
+diff    <- abs(dat_mpi - dat) / abs(dat_mpi)
 max(diff)
 EOF
 
