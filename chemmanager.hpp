dd2111797b3e6190966d40e1c1963d3bc285d84b
kohnakagawa <tsunekou1019@gmail.com>
2016-02-10 23:33:54 +0900

    backup commit

--- chemmanager.hpp
+++ chemmanager.hpp
@@ -22,6 +22,8 @@ public:
 //NOTE: PRNG is not thread safe.
 template<class FP>
 class ChemManager {
+  PS::ReallocatableArray<PS::U32> target_id;
+  
   static inline void Normalize(PS::F64vec& vec) {
     vec / std::sqrt(vec * vec);
   }
@@ -30,12 +32,10 @@ class ChemManager {
     return std::sqrt(-2.0 * Parameter::Tempera * std::log(PS::MT::genrand_real3() ) ) * std::cos(2.0 * M_PI * PS::MT::genrand_real3() );
   }
 
-  static inline void ApplyPBC(PS::F64vec& pos,
-			      const PS::F64vec& box_leng,
-			      const PS::F64vec& ibox_leng) {
-    pos.x -= std::floor(pos.x * ibox_leng.x) * box_leng.x;
-    pos.y -= std::floor(pos.y * ibox_leng.y) * box_leng.y;
-    pos.z -= std::floor(pos.z * ibox_leng.z) * box_leng.z;
+  static inline void ApplyPBC(PS::F64vec& pos) {
+    pos.x -= std::floor(pos.x * Parameter::ibox_leng.x) * Parameter::box_leng.x;
+    pos.y -= std::floor(pos.y * Parameter::ibox_leng.y) * Parameter::box_leng.y;
+    pos.z -= std::floor(pos.z * Parameter::ibox_leng.z) * Parameter::box_leng.z;
   }
 
   template<PS::U32 part_num, PS::U32 unit_beg>
@@ -44,7 +44,6 @@ class ChemManager {
 		     PS::U32 tpl_beg_id,
 		     PS::U32& new_ptcl_id, const PS::U32 new_amp_id,
 		     const PS::F64vec& tang_vec,
-		     const PS::F64vec& box_leng, const PS::F64vec& ibox_leng,
 		     PS::ReallocatableArray<PS::U32>& glob_topol) {
     FP new_ptcl;
     new_ptcl.prop = prop;
@@ -56,7 +55,7 @@ class ChemManager {
       new_ptcl.id = new_ptcl_id;
       new_ptcl.unit = unit_beg + i;
       new_ptcl.pos = sys[base_pid].pos + tang_vec;
-      ApplyPBC(new_ptcl.pos, box_leng, ibox_leng);
+      ApplyPBC(new_ptcl.pos);
       new_ptcl.delta_sumr = 0.0;
       
       new_ptcl.vel.x = GenThermalVeloc();
@@ -77,10 +76,106 @@ class ChemManager {
       new_ptcl_id++;
     }
   }
+
+  template<PS::U32 part_num, PS::U32 unit_beg>
+  void CreateAmpPart(PS::ParticleSystem<FP>& sys,
+		     const PS::U32 prop,
+		     PS::U32& new_ptcl_id,
+		     const PS::U32 new_amp_id,
+		     const PS::F64vec& copied_pos,
+		     const PS::F64vec& h2e) {
+    FP new_ptcl;
+    new_ptcl.prop = prop;
+    new_ptcl.amp_id = new_amp_id;
+    
+    for (PS::U32 i = 0; i < part_num; i++) {
+      new_ptcl.id   = new_ptcl_id;
+      new_ptcl.unit = unit_beg + i;
+      new_ptcl.pos  = copied_pos + h2e * (unit_beg + i);
+      ApplyPBC(new_ptcl.pos);
+      new_ptcl.delta_sumr = 0.0;
+
+      new_ptcl.vel.x = GenThermalVeloc();
+      new_ptcl.vel.y = GenThermalVeloc();
+      new_ptcl.vel.z = GenThermalVeloc();
+
+      new_ptcl.vel_buf = 0.0;
+      new_ptcl.acc = 0.0;
+      new_ptcl.press = 0.0;
+
+      new_ptcl.density.fill(0.0);
+
+      sys.AddNewParticle(new_ptcl);
+      
+      new_ptcl_id++;
+    }
+  }
+  
+  void DetermineTargetId(const PS::U32 amp_num,
+			 const PS::ReallocatableArray<PS::U32>& topol,
+			 const PS::U32 loc_num,
+			 Parameter& param) {
+    for (PS::U32 i = 0; i < amp_num; i++) {
+      const PS::F64 rnd = PS::MT::genrand_real1();
+      const PS::U32 head_id = topol[Parameter::all_unit * i];
+      if (rnd <= param.p_thresld && head_id != 0xffffffff && head_id < loc_num) {
+	const PS::U32 tail_id = topol[Parameter::all_unit * i + 2];
+	target_id.push_back(head_id);
+	target_id.push_back(tail_id);
+      }
+    }
+  }
   
 public:
   ChemManager(const PS::U32 seed) {
     PS::MT::init_genrand(seed);
+  }
+  
+  bool RandomChemEvent(PS::ParticleSystem<FP>& sys,
+		       const PS::ReallocatableArray<EPJ::DPD>& epj_org,
+		       const PS::ReallocatableArray<PS::U32>& loc_topol_cmpl,
+		       const PS::ReallocatableArray<PS::U32>& loc_topol_imcmpl,
+		       const PS::U32 cmplt_amp,
+		       const PS::U32 imcmplt_amp,
+		       Parameter& param) {
+    target_id.clearSize();
+
+    const PS::U32 loc_num = sys.getNumberOfParticleLocal();
+    DetermineTargetId(cmplt_amp, loc_topol_cmpl, loc_num, param);
+    DetermineTargetId(imcmplt_amp, loc_topol_imcmpl, loc_num, param);
+    const PS::U32 incr_num = target_id.size() / 2;
+    PS::U32 offset = 0; //amphiphile offset number
+    PS::Comm::exScan(&incr_num, &offset, 1);
+    PS::U32 new_ptcl_id = sys.getNumberOfParticleGlobal() + offset * Parameter::all_unit;
+    offset += param.amp_num; // add global offset
+    
+    for(PS::U32 i = 0; i < incr_num; i++) {
+      PS::F64vec h2e = epj_org[target_id[2 * i]].pos - epj_org[target_id[2 * i + 1]].pos;
+      ForceBonded<PS::ParticleSystem<FP> >::MinImage(h2e);
+      PS::F64vec tang_vec(PS::MT::genrand_real1(), PS::MT::genrand_real1(), 0.0);
+      tang_vec.z = -(h2e.x * tang_vec.x + h2e.y * tang_vec.y) / h2e.z;
+      Normalize(tang_vec);
+      Normalize(h2e);
+      tang_vec *= param.eps;
+      h2e *= Parameter::bond_leng;
+
+      const PS::F64vec copied_pos = tang_vec + epj_org[target_id[2 * i]].pos;
+      const PS::U32 new_amp_id = offset + i;
+      CreateAmpPart<Parameter::head_unit, 0                   >(sys, Parameter::Hyphil, new_ptcl_id,
+								new_amp_id, copied_pos, h2e);
+      CreateAmpPart<Parameter::tail_unit, Parameter::head_unit>(sys, Parameter::Hyphob, new_ptcl_id,
+								new_amp_id, copied_pos, h2e);
+    }
+
+    param.amp_num += PS::Comm::getSum(incr_num);
+
+    if(param.amp_num >= param.max_amp_num) {
+      std::cout << "Now param.amp_num >= param.max_amp_num.\n";
+      std::cout << "So, simulation will stop.\n";
+      return false;
+    } else {
+      return true;
+    }
   }
   
   bool RandomChemEvent(PS::ParticleSystem<FP>& sys,
@@ -105,11 +200,9 @@ public:
 	const PS::U32 tpl_beg_id = Parameter::all_unit * i;
 	CreateAmpPart<Parameter::head_unit, 0                   >(sys, Parameter::Hyphil, tpl_beg_id,
 								  new_ptcl_id, new_amp_id, tang_vec,
-								  param.box_leng, param.ibox_leng,
 								  glob_topol);
 	CreateAmpPart<Parameter::tail_unit, Parameter::head_unit>(sys, Parameter::Hyphob, tpl_beg_id + Parameter::head_unit,
 								  new_ptcl_id, new_amp_id, tang_vec,
-								  param.box_leng, param.ibox_leng,
 								  glob_topol);
 	param.amp_num++;
 	new_amp_id++;
