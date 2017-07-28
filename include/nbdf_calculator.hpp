039d6ebc85a0900208d7500cc80dbee381ebe916
kohnakagawa <tsunekou1019@gmail.com>
2017-07-28 16:20:58 +0900

    fix indent

--- include/nbdf_calculator.hpp
+++ include/nbdf_calculator.hpp
@@ -60,7 +60,7 @@ struct CalcDensity {
 struct CalcForceEpEpDPD {
   //prng seed
   static PS::U32 m_seed;
-    
+
   void operator () (const EPI::DPD* __restrict epi,
 		    const PS::S32 ni,
 		    const EPJ::DPD* __restrict epj,
@@ -73,70 +73,70 @@ struct CalcForceEpEpDPD {
       const PS::U32   idi = epi[i].id;
       const PS::U32 propi = epi[i].prop;
       const std::array<PS::F64, Parameter::prop_num> densi = epi[i].dens;
-      
+
       PS::F64vec fsum(0.0, 0.0, 0.0);
       PS::F64vec psum(0.0, 0.0, 0.0);
-      
+
       for (PS::S32 j = 0; j < nj; j++) {
-	if (idi == epj[j].id) continue;
-	
-	const PS::F64vec drij = ri - epj[j].pos;
-	const PS::F64 dr2 = drij * drij;
-	if (dr2 < Parameter::rc2) {
-	  const PS::F64vec dvij = vi - epj[j].vel;
-	  PS::F64 densij[Parameter::prop_num];
+        if (idi == epj[j].id) continue;
 
-	  for (PS::S32 k = 0; k < Parameter::prop_num; k++)
-	    densij[k] = densi[k] + epj[j].dens[k];
-	  
-	  const PS::U32 idj = epj[j].id;
-	  
-	  PS::U32 m_i = idi, m_j = idj;
-	  if (idi > idj){ // m_i <= m_j 
-	    m_i = idj;
-	    m_j = idi;
-	  }
+        const PS::F64vec drij = ri - epj[j].pos;
+        const PS::F64 dr2 = drij * drij;
+        if (dr2 < Parameter::rc2) {
+          const PS::F64vec dvij = vi - epj[j].vel;
+          PS::F64 densij[Parameter::prop_num];
 
-	  Saru saru(m_i, m_j, m_seed + Parameter::time);
-	  const PS::F64 rnd = saru.nrml();
+          for (PS::S32 k = 0; k < Parameter::prop_num; k++)
+            densij[k] = densi[k] + epj[j].dens[k];
 
-	  //kernel for thermostat
-	  const PS::F64 dr = std::sqrt(dr2);
-	  const PS::F64 inv_dr = 1.0 / dr;
-	  const PS::U32 propj  = epj[j].prop;
-	  const PS::F64 one_m_dr = 1.0 - dr * Parameter::irc;
+          const PS::U32 idj = epj[j].id;
 
-	  //kernel for conservative force
+          PS::U32 m_i = idi, m_j = idj;
+          if (idi > idj){ // m_i <= m_j
+            m_i = idj;
+            m_j = idi;
+          }
+
+          Saru saru(m_i, m_j, m_seed + Parameter::time);
+          const PS::F64 rnd = saru.nrml();
+
+          //kernel for thermostat
+          const PS::F64 dr = std::sqrt(dr2);
+          const PS::F64 inv_dr = 1.0 / dr;
+          const PS::U32 propj  = epj[j].prop;
+          const PS::F64 one_m_dr = 1.0 - dr * Parameter::irc;
+
+          //kernel for conservative force
 #ifdef PAIRWISE_DPD
-	  const PS::F64 cf_co = 25.0 * one_m_dr;
-	  const PS::F64 cf_mbd = 0.0;
-	  assert(std::isfinite(rnd));
+          (void) densij;
+          const PS::F64 cf_co = 25.0 * one_m_dr;
+          const PS::F64 cf_mbd = 0.0;
+          assert(std::isfinite(rnd));
 #else
+          const PS::F64 cf_co  = Parameter::cf_c[propi][propj] * (dr - Parameter::arc) * (dr - Parameter::rc) * (dr >= Parameter::arc);
+          PS::F64 cf_mbd = 0.0;
 
-	  const PS::F64 cf_co  = Parameter::cf_c[propi][propj] * (dr - Parameter::arc) * (dr - Parameter::rc) * (dr >= Parameter::arc);
-	  PS::F64 cf_mbd = 0.0;
-
-	  for (PS::S32 k = 0; k < Parameter::prop_num; k++)
-	    cf_mbd += densij[k] * Parameter::cf_m[propi][propj][k];
-	  cf_mbd *= one_m_dr;
+          for (PS::S32 k = 0; k < Parameter::prop_num; k++)
+            cf_mbd += densij[k] * Parameter::cf_m[propi][propj][k];
+          cf_mbd *= one_m_dr;
 #endif
 
-	  const PS::F64 wrij = one_m_dr; // pow = 1
-	  //const PS::F64 wrij = std::sqrt(one_m_dr); //pow = 1 / 2
-	  const PS::F64 sq_wrij = std::sqrt(wrij);
-	  
-	  const PS::F64 drij_dvij = drij * dvij;
-	  
-	  const PS::F64 all_cf = (cf_co + cf_mbd +  //conservative
-				  Parameter::cf_r[propi][propj] * sq_wrij * rnd - //random
-				  Parameter::cf_g[propi][propj] * wrij * drij_dvij * inv_dr) * inv_dr; //dissipation
+          const PS::F64 wrij = one_m_dr; // pow = 1
+          //const PS::F64 wrij = std::sqrt(one_m_dr); //pow = 1 / 2
+          const PS::F64 sq_wrij = std::sqrt(wrij);
 
-	  const PS::F64vec dF(all_cf * drij.x, all_cf * drij.y, all_cf * drij.z);
-	  const PS::F64vec dP(dF.x * drij.x, dF.y * drij.y, dF.z * drij.z);
-	  
-	  fsum += dF;
-	  psum += dP;
-	}
+          const PS::F64 drij_dvij = drij * dvij;
+
+          const PS::F64 all_cf = (cf_co + cf_mbd +  //conservative
+                                  Parameter::cf_r[propi][propj] * sq_wrij * rnd - //random
+                                  Parameter::cf_g[propi][propj] * wrij * drij_dvij * inv_dr) * inv_dr; //dissipation
+
+          const PS::F64vec dF(all_cf * drij.x, all_cf * drij.y, all_cf * drij.z);
+          const PS::F64vec dP(dF.x * drij.x, dF.y * drij.y, dF.z * drij.z);
+
+          fsum += dF;
+          psum += dP;
+        }
       }
       result[i].acc += fsum;
       result[i].press += psum;
