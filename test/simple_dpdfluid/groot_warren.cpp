fae3a3775724bab65bf7e71311739c0b09451c7b
kohnakagawa <tsunekou1019@gmail.com>
2017-08-03 14:25:45 +0900

    変数名変更に伴いコンパイルできなくなっていたので、コンパイルできるように修正。

--- test/simple_dpdfluid/groot_warren.cpp
+++ test/simple_dpdfluid/groot_warren.cpp
@@ -10,12 +10,17 @@
 #error "MPI is not supported yet!"
 #endif
 
+constexpr char Parameter::atom_type[21];
+PS::F64vec Parameter::box_leng, Parameter::ibox_leng;
+PS::U32 Parameter::time;
+PS::U32 Parameter::all_time, Parameter::step_mic, Parameter::step_mac;
+
 static_assert(Parameter::all_unit == 4, "all_unit should be 4.");
 
 namespace {
   void calc_mean_pressure(const std::string& cdir,
-			  const PS::S32 beg,
-			  const PS::F64 rho)
+                          const PS::S32 beg,
+                          const PS::F64 rho)
   {
     std::string fname = cdir + "/pressure.txt";
     std::ifstream fin(fname.c_str());
@@ -23,17 +28,17 @@ namespace {
 
     PS::F64vec press_mean(0.0, 0.0, 0.0), buf(0.0, 0.0, 0.0);
     PS::S32 cnt = 0, time = 0;
-    
+
     while(true) {
       fin >> buf;
       if(fin.eof()) break;
       if(time >= beg) {
-	press_mean += buf;
-	cnt++;
+        press_mean += buf;
+        cnt++;
       }
       time++;
     }
-    
+
     press_mean /= cnt;
     const PS::F64 press = (press_mean.x + press_mean.y + press_mean.z) / 3.0;
     fname = "./rho_vs_press.txt";
@@ -43,8 +48,8 @@ namespace {
   }
 
   void calc_mean_kintemp(const std::string& cdir,
-			 const PS::S32 beg,
-			 const PS::F64 dt)
+                         const PS::S32 beg,
+                         const PS::F64 dt)
   {
     std::string fname = cdir + "/kin_temp.txt";
     std::ifstream fin(fname.c_str());
@@ -52,13 +57,13 @@ namespace {
 
     PS::F64 T_mean = 0.0, buf = 0.0;
     PS::S32 cnt = 0, time = 0;
-    
-    while(true) {
+
+    while (true) {
       fin >> buf;
       if(fin.eof()) break;
       if(time >= beg) {
-	T_mean += buf;
-	cnt++;
+        T_mean += buf;
+        cnt++;
       }
       time++;
     }
@@ -77,45 +82,45 @@ namespace {
     F_CALC,
     KICK,
   };
-  
+
   template<class Tpsys>
   bool check_value_is_finite(Tpsys& system, const PS::U32 id) {
     const PS::U32 num = system.getNumberOfParticleLocal();
     for(PS::U32 i = 0; i < num; i++) {
       const bool pos_valid = (std::isfinite(system[i].pos.x) &&
-			      std::isfinite(system[i].pos.y) &&
-			      std::isfinite(system[i].pos.z));
+                              std::isfinite(system[i].pos.y) &&
+                              std::isfinite(system[i].pos.z));
       const bool vel_valid = (std::isfinite(system[i].vel.x) &&
-			      std::isfinite(system[i].vel.y) &&
-			      std::isfinite(system[i].vel.z));
+                              std::isfinite(system[i].vel.y) &&
+                              std::isfinite(system[i].vel.z));
       const bool acc_valid = (std::isfinite(system[i].acc.x) &&
-			      std::isfinite(system[i].acc.y) &&
-			      std::isfinite(system[i].acc.z));
+                              std::isfinite(system[i].acc.y) &&
+                              std::isfinite(system[i].acc.z));
       if(!pos_valid)
-	std::cerr << "id " << id << " position is not finite value.\n";
+        std::cerr << "id " << id << " position is not finite value.\n";
 
       if(!vel_valid)
-	std::cerr << "id " << id << " velocity is not finite value.\n";
+        std::cerr << "id " << id << " velocity is not finite value.\n";
 
       if(!acc_valid)
-	std::cerr << "id " << id << " force is not finite value.\n";
-	
+        std::cerr << "id " << id << " force is not finite value.\n";
+
       if( (!pos_valid) || (!vel_valid) || (!acc_valid) ) {
-	std::cerr << "id: " << system[i].id << std::endl;
-	std::cerr << "pos: " << system[i].pos << std::endl;
-	std::cerr << "vel: " << system[i].vel << std::endl;
-	std::cerr << "acc: " << system[i].acc << std::endl;
-	return false;
+        std::cerr << "id: " << system[i].id << std::endl;
+        std::cerr << "pos: " << system[i].pos << std::endl;
+        std::cerr << "vel: " << system[i].vel << std::endl;
+        std::cerr << "acc: " << system[i].acc << std::endl;
+        return false;
       }
     }
     return true;
   }
 };
 
-#define CHECK_VAL(obj, id)						\
-  if(!check_value_is_finite((obj), (id))) {				\
+#define CHECK_VAL(obj, id)                                              \
+  if(!check_value_is_finite((obj), (id))) {                             \
     std::cerr << "Error occurs at " << __FILE__ << " " << __LINE__ << std::endl; \
-    PS::Abort();							\
+    PS::Abort();                                                        \
   }
 
 int main(int argc, char *argv[]) {
@@ -126,19 +131,25 @@ int main(int argc, char *argv[]) {
     std::cerr << "argv[3] = run mode.\n";
     PS::Abort();
   }
-  
+
   const std::string cdir = argv[1];
   Parameter param(cdir);
   param.Initialize();
-  param.LoadParam();
-  param.CheckLoaded();
+  if (PS::Comm::getRank() == 0) param.LoadParam();
 
   PS::ParticleSystem<FPDPD> system;
   system.initialize();
-  system.setNumberOfParticleLocal(param.init_amp_num * Parameter::all_unit);
-  
-  //load particle configuration.
-  param.LoadParticleConfig(system);
+  if (PS::Comm::getRank() == 0) {
+    system.setNumberOfParticleLocal(param.init_amp_num * Parameter::all_unit);
+    Parameter::time = param.LoadParticleConfig(system);
+  } else {
+    system.setNumberOfParticleLocal(0);
+  }
+
+  if (PS::Comm::getRank() == 0) param.CalcCorePtclId(system);
+
+  param.ShareDataWithOtherProc();
+  param.CheckLoaded();
   param.CheckParticleConfigIsValid(system);
 
   PS::DomainInfo dinfo;
@@ -147,29 +158,36 @@ int main(int argc, char *argv[]) {
   dinfo.setBoundaryCondition(PS::BOUNDARY_CONDITION_PERIODIC_XYZ);
   dinfo.setPosRootDomain(PS::F64vec(0.0, 0.0, 0.0), Parameter::box_leng);
   dinfo.collectSampleParticle(system);
-  dinfo.decomposeDomain();
+  dinfo.decomposeDomainAll(system);
   system.exchangeParticle(dinfo);
 
+  // Initial step
+  drift_and_predict(system, param.dt, Parameter::box_leng, Parameter::ibox_leng);
+  dinfo.decomposeDomainAll(system);
+  system.exchangeParticle(dinfo);
   PS::TreeForForceShort<RESULT::Density, EPI::Density, EPJ::Density>::Gather dens_tree;
-  dens_tree.initialize(3 * system.getNumberOfParticleGlobal() );
-  dens_tree.calcForceAllAndWriteBack(CalcDensity(), system, dinfo);
-
   PS::TreeForForceShort<RESULT::ForceDPD, EPI::DPD, EPJ::DPD>::Gather force_tree;
+  dens_tree.initialize(3 * system.getNumberOfParticleGlobal() );
   force_tree.initialize(3 * system.getNumberOfParticleGlobal() );
+
+  CalcForceEpEpDPD::m_seed = static_cast<PS::U32>(time(nullptr));
+  dens_tree.calcForceAllAndWriteBack(CalcDensity(), system, dinfo);
   force_tree.calcForceAllAndWriteBack(CalcForceEpEpDPD(), system, dinfo);
   CHECK_VAL(system, F_CALC);
 
   // observer
   Observer<PS::ParticleSystem<FPDPD> > observer(cdir);
   observer.Initialize();
-  
+
+  kick(system, param.dt);
+
   //main loop
   PS::F64vec buf(0.0, 0.0, 0.0);
-  for(Parameter::time = 0; Parameter::time < Parameter::all_time; Parameter::time++) {
+  for (Parameter::time = 0; Parameter::time < Parameter::all_time; Parameter::time++) {
     drift_and_predict(system, param.dt, param.box_leng, param.ibox_leng);
     CHECK_VAL(system, DRIFT_KICK);
-    
-    dinfo.decomposeDomain();
+
+    dinfo.decomposeDomainAll(system);
     CHECK_VAL(system, DECOMPOSE);
 
     system.exchangeParticle(dinfo);
@@ -178,14 +196,14 @@ int main(int argc, char *argv[]) {
     dens_tree.calcForceAllAndWriteBack(CalcDensity(), system, dinfo);
     force_tree.calcForceAllAndWriteBack(CalcForceEpEpDPD(), system, dinfo);
     CHECK_VAL(system, F_CALC);
-    
+
     kick(system, param.dt);
     CHECK_VAL(system, KICK);
 
     if(Parameter::time % Parameter::step_mac == 0) {
       observer.KineticTempera(system);
       observer.Pressure(system, buf, param.ibox_leng);
-      observer.Diffusion(system, param.amp_num);
+      observer.Diffusion(system, param.amp_num, param.sol_num);
       //observer.ConfigTempera();
     }
 
@@ -199,11 +217,11 @@ int main(int argc, char *argv[]) {
 
   observer.FinConfig(system);
   observer.CleanUp();
-  param.DumpAllParam();
-  
+  param.DebugDumpAllParam();
+
   const PS::S32 beg_time = std::atoi(argv[2]);
   const std::string mode = argv[3];
-  
+
   if(mode == "pressure") {
     const PS::F64 rho = system.getNumberOfParticleGlobal() / (param.box_leng.x * param.box_leng.y * param.box_leng.z);
     calc_mean_pressure(cdir, beg_time, rho);
@@ -213,6 +231,6 @@ int main(int argc, char *argv[]) {
     std::cerr << "Unknown mode.\n";
     PS::Abort();
   }
-  
+
   PS::Finalize();
 }
