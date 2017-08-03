318ba29e6732f7724de82e5b5552a72c7554e2a8
kohnakagawa <tsunekou1019@gmail.com>
2017-08-03 14:24:46 +0900

    気液共存の場合のテストシミュレーションを行うためのツールを追加。

---
+++ test/mdpd_fluid/mean.R
@@ -0,0 +1,24 @@
+load.pressure <- function(root.dir) {
+  fname <- file.path(root.dir, "pressure.txt")
+  pxyz <- data.matrix(read.table(fname))
+  return (pxyz)
+}
+
+slice <- function(vec, begin) {
+  return (vec[seq(begin, length(vec))])
+}
+
+calc.pressure <- function(pxyz, begin) {
+  px <- mean(slice(pxyz[,1], begin))
+  py <- mean(slice(pxyz[,2], begin))
+  pz <- mean(slice(pxyz[,3], begin))
+  p.tot <- mean(slice((pxyz[,1] + pxyz[,2] + pxyz[,3]) / 3.0, begin))
+  return (c(px, py, pz, p.tot))
+}
+
+observe.beg <- 30000
+print(calc.pressure(load.pressure("./mol_dens20.0"), observe.beg))
+print(calc.pressure(load.pressure("./mol_dens30.0"), observe.beg))
+print(calc.pressure(load.pressure("./mol_dens40.0"), observe.beg))
+print(calc.pressure(load.pressure("./mol_dens50.0"), observe.beg))
+print(calc.pressure(load.pressure("./mol_dens60.0"), observe.beg))
