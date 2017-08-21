822b5014ee2b4c20380f652ab3a757d2c627ca50
kohnakagawa <tsunekou1019@gmail.com>
2017-08-21 11:43:08 +0900

    MPI版 parameter includeとbond bendのテスト

--- test/mpi_io/io_test.cpp
+++ test/mpi_io/io_test.cpp
@@ -13,23 +13,30 @@ PS::U32 Parameter::all_time, Parameter::step_mic, Parameter::step_mac;
 
 int main(int argc, char *argv[]) {
   PS::Initialize(argc, argv);
+  if (argc != 2) {
+    std::cerr << "argv[1] = target directory.\n";
+    PS::Abort();
+  }
 
-  const std::string cdir = argv[1];  
+  const std::string cdir = argv[1];
   Parameter param(cdir);
   param.Initialize();
-  if(PS::Comm::getRank() == 0) param.LoadParam();
-  
+  if (PS::Comm::getRank() == 0) param.LoadParam();
+
   PS::ParticleSystem<FPDPD> system;
   system.initialize();
   if(PS::Comm::getRank() == 0) {
-    system.setNumberOfParticleLocal(param.init_amp_num * Parameter::all_unit);
+    system.setNumberOfParticleLocal(param.init_amp_num * Parameter::all_unit + param.sol_num);
     Parameter::time = param.LoadParticleConfig(system);
   } else {
-    system.setNumberOfParticleLocal(0);    
+    system.setNumberOfParticleLocal(0);
   }
+
+  if (PS::Comm::getRank() == 0) param.CalcCorePtclId(system);
 
   param.ShareDataWithOtherProc();
   param.CheckLoaded();
+  param.CheckParticleConfigIsValid(system);
   param.DebugDumpAllParam();
 
   PS::DomainInfo dinfo;
@@ -37,15 +44,14 @@ int main(int argc, char *argv[]) {
   dinfo.initialize(coef_ema);
   dinfo.setBoundaryCondition(PS::BOUNDARY_CONDITION_PERIODIC_XYZ);
   dinfo.setPosRootDomain(PS::F64vec(0.0, 0.0, 0.0), Parameter::box_leng);
-  dinfo.collectSampleParticle(system);
-  dinfo.decomposeDomain();
+  dinfo.decomposeDomainAll(system);
   system.exchangeParticle(dinfo);
 
   std::cout << system.getNumberOfParticleLocal() << std::endl;;
-  
+
   Observer<PS::ParticleSystem<FPDPD> > observer(cdir);
   observer.Initialize();
   observer.Trajectory(system);
-  
+
   PS::Finalize();
 }
--- test/mpi_io/run_test.sh
+++ test/mpi_io/run_test.sh
@@ -7,3 +7,7 @@ awk '{if(NR >= 3) print $0}' ./result/init_config.xyz > org.txt
 sort -n -k5 tmp.txt > sort.txt
 diff sort.txt org.txt
 rm tmp.txt org.txt sort.txt
+
+diff ./result/debug_dump_rank0.txt ./result/debug_dump_rank1.txt
+diff ./result/debug_dump_rank1.txt ./result/debug_dump_rank2.txt
+diff ./result/debug_dump_rank2.txt ./result/debug_dump_rank3.txt
